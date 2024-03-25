f2a45a160cdbfb0b83e0a330587c8ce25f8e8c22
Sergiu mihuleac <mihuleac.sergiu@gmail.com>
2024-03-25 15:53:41 +0200

    update

--- api_atbqt/scraper.py
+++ api_atbqt/scraper.py
@@ -1,8 +1,13 @@
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 import logging.config
+import requests
+import socket
 import redis
 import time
 
@@ -15,19 +20,47 @@ def setup_options():
         options.page_load_strategy = "normal"
         return options
 
+status_code_script = """
+var callback = arguments[arguments.length - 1];
+var xhr = new XMLHttpRequest();
+xhr.open('GET', window.location.href, true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4) {
+    callback(xhr.status);
+  }
+};
+xhr.send();
+"""
+
+script_ip = """
+function getPublicIP() {
+    fetch('https://api.bigdatacloud.net/data/client-ip')  // Or your chosen service
+        .then(response => response.json())
+        .then(data => {
+            const ipAddress = data.ipString;
+            console.log("Node's Public IP Address:", ipAddress);
+            // Optionally, return the IP address back to your Python code
+            return ipAddress; 
+        })
+        .catch(error => console.error("Error fetching IP:", error));
+}
+
+return getPublicIP(); 
+"""
+
 def read_data(driver, num_of_tabs, redis_con, semaphore_for_redis, parse_options, request_id):
     for tab_index in range(num_of_tabs):
         driver.switch_to.window(driver.window_handles[tab_index])
+        if parse_options.slow:
+            driver.execute_script("window.scrollBy(0,950)")
+            time.sleep(1)
         if parse_options.scroll:
             for _ in range(parse_options.scroll_amount):
-                driver.execute_script("window.scrollBy(0,650)")
+                driver.execute_script("window.scrollBy(0,950)")
         result = driver.page_source
-        if parse_options.slow:
-            time.sleep(2)
         
         with semaphore_for_redis:
             redis_con.hset(f'page_sources:{request_id}', driver.current_url, result)
-    # driver.quit()
 
 def get_data(id, parse_options, group_of_tabs, total_num, semaphore_for_driver, semaphore_for_redis): # in thread
     redis_con = redis.Redis(host='192.168.81.108', port=6379, db=0)
@@ -39,12 +72,23 @@ def get_data(id, parse_options, group_of_tabs, total_num, semaphore_for_driver, 
         try:
             if index == 0:
                 driver.get(url)
-                logging.info(f'First tab: {url} - Title: {driver.title}')
+                status_code = driver.execute_async_script(status_code_script)
+                logging.info(f'{status_code} First tab: {url} - Title: {driver.title}')
             else:
                 driver.execute_script("window.open('');")
                 driver.switch_to.window(driver.window_handles[index])
                 driver.get(url)
-                logging.info(f'tab: {index} {url} - Title: {driver.title}')
+                status_code = driver.execute_async_script(status_code_script)
+                # status_code = WebDriverWait(driver, 10).until(lambda d: d.execute_async_script(status_code_script))
+                logging.info(f' {status_code} Tab: {index} {url} - Title: {driver.title}')
+            if status_code != 200:
+                # public_ip = driver.execute_script(script_ip)
+                public_ip = WebDriverWait(driver, 10).until(lambda d: d.execute_async_script(script_ip))
+                logging.error(f'{status_code} for {url} - Title: {driver.title}')
+                logging.error(f'My IP: {public_ip}')
+                logging.warning(f'{driver.get_log('browser')}')
+                for entry in driver.get_log('browser'):
+                    logging.warning(entry['message'])
             with semaphore_for_redis:
                 redis_con.sadd(processed_key, url)
         except WebDriverException as e:
@@ -57,11 +101,5 @@ def get_data(id, parse_options, group_of_tabs, total_num, semaphore_for_driver, 
         num_processed_links = redis_con.scard(processed_key)
         num_failed_links = redis_con.scard(failed_key)
         if num_processed_links + num_failed_links == total_num:
-            # Set a flag in Redis to indicate the process has finished
             redis_con.set(f'finished:{id}', 1)    
     driver.quit()
-    # if parse_options.scroll:
-    #     pass
-        # logging.warning(url)
-    # r.ping()
-    # print(id, options, group_of_tabs, total_num)
